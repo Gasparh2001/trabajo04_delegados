b5fdb5498224c67c4cddab3fc34e52dcdfd5000e
Gasparh2001 <gasparh2001@gmail.com>
2023-12-02 14:37:03 +0100

    primer script propio

--- Assets/Scripts/Events/EventsGameManager.cs
+++ Assets/Scripts/Events/EventsGameManager.cs
@@ -3,4 +3,10 @@ using UnityEngine;
 public class EventsGameManager : MonoBehaviour
 {
     // HERE YOU WILL HAVE TO DECLARE AND INVOKE THE EVENTS
+
+
+    public delegate void ClickPress();
+
+    public static event ClickPress OnEnterKeyPressed;//active when press retur
+    public static event ClickPress OnEscapeKeyPressed;//active when press escape
 }
